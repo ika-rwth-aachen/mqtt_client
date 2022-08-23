de9916baafb6e09020156f09582ddcf7a9f96b71
buller <marco.buller@ika.rwth-aachen.de>
2022-08-23 11:25:42 +0200

    Compilable basic structure without functionalities

--- include/mqtt_client/MqttClient.h
+++ include/mqtt_client/MqttClient.h
@@ -24,3 +24,44 @@ SOFTWARE.
 ==============================================================================
 */
 
+#pragma once
+
+#include <filesystem>
+#include <map>
+#include <memory>
+#include <string>
+
+#include <mqtt/async_client.h>
+#include "rclcpp/rclcpp.hpp"
+
+#include "rclcpp/logger.hpp"
+#include "rcutils/logging_macros.h"
+#include "rcpputils/filesystem_helper.hpp"
+
+
+/**
+ * Namespace for the mqtt_client package
+ */
+namespace mqtt_client {
+
+
+/**
+ * ROS Nodelet for sending and receiving ROS messages via MQTT
+ *
+ * The MqttClient enables connected ROS-based devices or robots to
+ * exchange ROS messages via an MQTT broker using the MQTT protocol.
+ * This works generically for any ROS message, i.e. there is no need
+ * to specify the ROS message type for ROS messages you wish to
+ * exchange via the MQTT broker.
+ */
+class MqttClient : public rclcpp::Node,
+                   public virtual mqtt::callback,
+                   public virtual mqtt::iaction_listener{
+
+ public:
+  MqttClient();
+
+
+
+};
+} //Namespace
--- src/MqttClient.cpp
+++ src/MqttClient.cpp
@@ -28,43 +28,19 @@ SOFTWARE.
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
 
+#include "mqtt_client/MqttClient.h"
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float64.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+namespace mqtt_client{
 
-class MqttClient: public rclcpp::Node
-{
-  public:
-    MqttClient()
-    : Node("mqtt_client"), count_(0)
-    {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MqttClient::timer_callback, this));
-    }
+MqttClient::MqttClient() : Node("mqtt_client"){
 
-  private:
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
-};
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MqttClient>());
-  rclcpp::shutdown();
-  return 0;
 }
+
+} //Namespace
