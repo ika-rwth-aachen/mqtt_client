1f681d4d87e35a028b42c3bc37bf158b97cc5d5f
David B <david.b@smartmachine.nz>
2023-08-23 10:16:13 +1200

    fix: typo.

--- mqtt_client/include/mqtt_client/MqttClient.h
+++ mqtt_client/include/mqtt_client/MqttClient.h
@@ -31,7 +31,7 @@ SOFTWARE.
 #include <map>
 #include <memory>
 #include <string>
-#include <rosfmt/full> // fmt::format, fmt::join
+#include <rosfmt/full.h> // fmt::format, fmt::join
 
 #include <mqtt/async_client.h>
 #include <mqtt_client_interfaces/IsConnected.h>
