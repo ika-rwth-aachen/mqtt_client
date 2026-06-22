c7bebb3c88840b34c077b2848725311a68de2f0e
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-06-22 15:11:00 +0000

    keep node_name launch arg

--- mqtt_client/launch/standalone.launch.py
+++ mqtt_client/launch/standalone.launch.py
@@ -9,13 +9,13 @@ from tracetools_launch.action import Trace
 
 def generate_launch_description():
     namespace = LaunchConfiguration("namespace")
-    name = LaunchConfiguration("name")
+    node_name = LaunchConfiguration("node_name")
     ros_tracing = LaunchConfiguration("ros_tracing")
     params_file = LaunchConfiguration("params_file")
 
     return LaunchDescription([
         DeclareLaunchArgument("namespace", default_value=""),
-        DeclareLaunchArgument("name", default_value="mqtt_client"),
+        DeclareLaunchArgument("node_name", default_value="mqtt_client"),
         DeclareLaunchArgument(
             "params_file",
             default_value=PathJoinSubstitution([
@@ -26,7 +26,7 @@ def generate_launch_description():
         Node(
             package="mqtt_client",
             executable="mqtt_client",
-            name=name,
+            name=node_name,
             namespace=namespace,
             output="screen",
             parameters=[params_file],
