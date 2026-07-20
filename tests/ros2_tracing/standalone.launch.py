7e34adc0c38e96d9f3d33a6a88599327f6d490a4
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-07-20 17:40:29 +0200

    add test for tracing

---
+++ tests/ros2_tracing/standalone.launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from tracetools_launch.action import Trace
+
+
+def generate_launch_description():
+    namespace = LaunchConfiguration("namespace")
+    node_name = LaunchConfiguration("node_name")
+    ros_tracing = LaunchConfiguration("ros_tracing")
+    params_file = LaunchConfiguration("params_file")
+    ping_node = LaunchConfiguration("ping_node")
+
+    return LaunchDescription([
+        DeclareLaunchArgument("namespace", default_value=""),
+        DeclareLaunchArgument("node_name", default_value="mqtt_client"),
+        DeclareLaunchArgument(
+            "params_file",
+            default_value=PathJoinSubstitution([
+                FindPackageShare("mqtt_client"), "config", "params.yaml"
+            ]),
+        ),
+        DeclareLaunchArgument("ros_tracing", default_value="false", description="enable tracing"),
+        DeclareLaunchArgument("ping_node", default_value="false", description="enable ping node"),
+        ExecuteProcess(
+            cmd=["ros2", "topic", "pub", "/ping/ros", "std_msgs/msg/String", "data: ping"],
+            condition=IfCondition(ping_node),
+        ),
+        Node(
+            package="mqtt_client",
+            executable="mqtt_client",
+            name=node_name,
+            namespace=namespace,
+            output="screen",
+            parameters=[params_file],
+        ),
+        Trace(
+            session_name="trace",
+            dual_session=True,
+            condition=IfCondition(ros_tracing),
+        ),
+    ])
