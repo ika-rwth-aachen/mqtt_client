03a4601b42b4df14a5e17ae3b9f804f39da91e28
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-07-20 15:52:43 +0200

    build all packages form ros2_tracing

--- docker/custom.sh
+++ docker/custom.sh
@@ -3,7 +3,7 @@
 # 'message-link instrumentation' and 'dual-session mode' in jazzy
 cd /docker-ros/ws
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
-rosdep update && rosdep install -y -i --from-paths src/ros2_tracing/tracetools src/ros2_tracing/tracetools_launch
+rosdep update && rosdep install -y -i --from-paths src
 source /opt/ros/${ROS_DISTRO}/setup.bash
-colcon build --packages-up-to tracetools tracetools_launch --allow-overriding tracetools --allow-overriding tracetools_launch
+colcon build --allow-overriding tracetools --allow-overriding tracetools_launch
 rm -r src/ros2_tracing log build
